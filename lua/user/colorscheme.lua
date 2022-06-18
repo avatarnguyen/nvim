138024a27b46992a035b790eceb028382066d575
anhnguyen <anh.nguyen@knowunity.com>
2022-06-18 14:07:40 +0200

    add extra option to nightfly

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,6 @@
-local colorscheme = "tokyonight"
+local colorscheme = "nightfly"
+vim.g.nightflyItalics = 1
+vim.g.nightflyNormalFloat = 1
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
