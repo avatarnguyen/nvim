c3b8228b51bc276bc289bd86c8ffbffd56b74666
anhnguyen <anh.nguyen@knowunity.com>
2022-06-18 14:07:05 +0200

    migrate old config

---
+++ lua/user/tabnine.lua
@@ -0,0 +1,18 @@
+local cmp_status_ok, tabnine = pcall(require, "cmp_tabnine.config")
+if not cmp_status_ok then
+  return
+end
+
+tabnine:setup({
+	max_lines = 300;
+	max_num_results = 4;
+	sort = true;
+	run_on_every_keystroke = true;
+	snippet_placeholder = '..';
+	ignored_file_types = { -- default is not to ignore
+		-- uncomment to ignore in lua:
+    yaml = true,
+		lua = true,
+    log = true,
+	};
+})
