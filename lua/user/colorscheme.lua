342b05c99cf975f5261bc2334c5a22bd6745834d
anhnguyen <anh.nguyen@knowunity.com>
2022-06-21 23:27:51 +0200

    change to tokyonight theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,12 @@
-local colorscheme = "darkplus"
+local colorscheme = "tokyonight"
+
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_variables = 1
+vim.g.tokyonight_italic_keywords = 1
+vim.g.tokyonight_lualine_bold = 1
+vim.g.tokyonight_italic_functions = 1
+vim.g.tokyonight_dark_sidebar = 1
+-- vim.g.tokyonight_transparent = 0
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
