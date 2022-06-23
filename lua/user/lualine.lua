4e91f914dd780c1cd7aa7ece677b4dd910622dc5
anhnguyen <anh.nguyen@knowunity.com>
2022-06-23 12:51:13 +0200

    remove mode

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,72 +1,72 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-  return
+	return
 end
 
 local hide_in_width = function()
-  return vim.fn.winwidth(0) > 80
+	return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
-  "diagnostics",
-  sources = { "nvim_diagnostic" },
-  sections = { "error", "warn", 'info' },
-  symbols = { error = " ", warn = " ", info = " " },
-  colored = true,
-  update_in_insert = false,
-  always_visible = false,
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn", "info" },
+	symbols = { error = " ", warn = " ", info = " " },
+	colored = true,
+	update_in_insert = false,
+	always_visible = false,
 }
 
 local diff = {
-  "diff",
-  colored = true,
-  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = hide_in_width,
+	"diff",
+	colored = true,
+	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+	cond = hide_in_width,
 }
 
 local filetype = {
-  "filetype",
-  icons_enabled = false,
-  icon = nil,
+	"filetype",
+	icons_enabled = false,
+	icon = nil,
 }
 
 local branch = {
-  "branch",
-  icons_enabled = true,
-  icon = "",
+	"branch",
+	icons_enabled = true,
+	icon = "",
 }
 
 local location = {
-  "location",
-  padding = 1,
+	"location",
+	padding = 1,
 }
 
 local filepath = {
-  'filename',
-  file_status = true, -- displays file status (readonly status, modified status)
-  path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+	"filename",
+	file_status = true, -- displays file status (readonly status, modified status)
+	path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
 }
 
 local spaces = function()
-  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
-lualine.setup {
-  options = {
-    globalstatus = true,
-    icons_enabled = true,
-    theme = "auto",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = { "alpha", "dashboard" },
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = {branch},
-    lualine_c = { diagnostics, filepath },
-    lualine_x = { diff, spaces, "encoding", filetype },
-    lualine_y = { location },
-    lualine_z = { "progress" },
-  },
-}
+lualine.setup({
+	options = {
+		globalstatus = true,
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "alpha", "dashboard" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { branch },
+		lualine_b = { filepath },
+		lualine_c = { diagnostics },
+		lualine_x = { diff, spaces, "encoding", filetype },
+		lualine_y = { location },
+		lualine_z = { "progress" },
+	},
+})
