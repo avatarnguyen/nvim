612e1953072ee72175c5252014640f4849ce9403
anhnguyen <anh.nguyen@knowunity.com>
2022-06-25 13:54:38 +0200

    add json lsp, clean up

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -18,7 +18,7 @@ null_ls.setup {
     --   extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     -- },
     -- formatting.black.with { extra_args = { "--fast" } },
-    formatting.stylua,
+    -- formatting.stylua,
     -- formatting.google_java_format,
     -- diagnostics.flake8,
   },
