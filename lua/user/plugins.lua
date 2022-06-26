645d26023f0e7c4b2977de17afde573af5d2dd34
anhnguyen <anh.nguyen@knowunity.com>
2022-06-26 12:56:34 +0200

    update treesitter automatically

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -109,8 +109,8 @@ return packer.startup(function(use)
   })
 
   -- Treesitter
-  use("nvim-treesitter/nvim-treesitter")
-  -- use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  -- use("nvim-treesitter/nvim-treesitter")
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
   --	use("nvim-treesitter/nvim-treesitter-context")
 
