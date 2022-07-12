b980fa9f55affc42966992252ddea8cf39239947
anhnguyen <anh.nguyen@knowunity.com>
2022-07-12 22:21:56 +0200

    add colorscheme condition

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -11,37 +11,41 @@ local colorscheme = "kanagawa"
 --   " set fillchars=horiz:━,horizup:┻,horizdown:┳,vert:┃,vertleft:┨,vertright:┣,verthoriz:╋
 
 -- Kanagawa Colorscheme:
-require('kanagawa').setup({
-    undercurl = true,           -- enable undercurls
+if colorscheme == "kanagawa" then
+  require('kanagawa').setup({
+    undercurl = true, -- enable undercurls
     commentStyle = { italic = true },
     functionStyle = {},
-    keywordStyle = { italic = true},
+    keywordStyle = { italic = true },
     statementStyle = { bold = true },
     typeStyle = { bold = true },
-    variablebuiltinStyle = { italic = true},
-    specialReturn = true,       -- special highlight for the return keyword
-    specialException = true,    -- special highlight for exception handling keywords
-    transparent = false,        -- do not set background color
-    dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
-    globalStatus = false,       -- adjust window separators highlight for laststatus=3
+    variablebuiltinStyle = { italic = true },
+    specialReturn = true, -- special highlight for the return keyword
+    specialException = true, -- special highlight for exception handling keywords
+    transparent = false, -- do not set background color
+    dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+    globalStatus = false, -- adjust window separators highlight for laststatus=3
     colors = {},
     overrides = {},
-})
+  })
+end
 
--- Tokyonight Storm Colorscheme
--- vim.g.tokyonight_sidebars = [ "qf", "vista_kind", "terminal", "packer" ]
--- vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
--- vim.g.tokyonight_transparent_sidebar = 0
+if colorscheme == "tokyonight" then
+  -- Tokyonight Storm Colorscheme
+  -- vim.g.tokyonight_sidebars = [ "qf", "vista_kind", "terminal", "packer" ]
+  -- vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+  -- vim.g.tokyonight_transparent_sidebar = 0
 
--- Current Use Option
--- vim.g.tokyonight_style = "night"
--- vim.g.tokyonight_style = "storm"
-vim.g.tokyonight_italic_variables = 1
-vim.g.tokyonight_italic_keywords = 1
-vim.g.tokyonight_lualine_bold = 1
-vim.g.tokyonight_italic_functions = 1
-vim.g.tokyonight_transparent = 0
-vim.g.tokyonight_dark_sidebar = 1
+  -- Current Use Option
+  -- vim.g.tokyonight_style = "night"
+  -- vim.g.tokyonight_style = "storm"
+  vim.g.tokyonight_italic_variables = 1
+  vim.g.tokyonight_italic_keywords = 1
+  vim.g.tokyonight_lualine_bold = 1
+  vim.g.tokyonight_italic_functions = 1
+  vim.g.tokyonight_transparent = 0
+  vim.g.tokyonight_dark_sidebar = 1
+end
 
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
