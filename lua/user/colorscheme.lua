71545ad6d420b6c06f06fb76909ed1653c464c3c
anhnguyen <anh.nguyen@knowunity.com>
2022-07-17 11:43:13 +0200

    add material theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,8 @@
 -- local colorscheme = "kanagawa"
 -- local colorscheme = "tokyonight"
 -- local colorscheme = "cobalt2"
+-- local colorscheme = "material"
 local colorscheme = "nightfly"
--- local colorscheme = "night-owl"
 
 -- require('night-owl')
 
@@ -57,6 +57,96 @@ if colorscheme == "tokyonight" then
   vim.g.tokyonight_italic_functions = 1
   vim.g.tokyonight_transparent = 0
   vim.g.tokyonight_dark_sidebar = 1
+end
+
+if colorscheme == "material" then
+  vim.g.material_style = "deep ocean"
+  require('material').setup({
+    contrast = {
+      sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+      floating_windows = false, -- Enable contrast for floating windows
+      line_numbers = false, -- Enable contrast background for line numbers
+      sign_column = false, -- Enable contrast background for the sign column
+      cursor_line = false, -- Enable darker background for the cursor line
+      non_current_windows = false, -- Enable darker background for non-current windows
+      popup_menu = false, -- Enable lighter background for the popup menu
+    },
+
+    italics = {
+      comments = true, -- Enable italic comments
+      keywords = false, -- Enable italic keywords
+      functions = false, -- Enable italic functions
+      strings = false, -- Enable italic strings
+      variables = true -- Enable italic variables
+    },
+
+    contrast_filetypes = { -- Specify which filetypes get the contrasted (darker) background
+      "terminal", -- Darker terminal background
+      "packer", -- Darker packer background
+      "qf" -- Darker qf list background
+    },
+
+    high_visibility = {
+      lighter = false, -- Enable higher contrast text for lighter style
+      darker = false -- Enable higher contrast text for darker style
+    },
+
+    disable = {
+      colored_cursor = false, -- Disable the colored cursor
+      borders = false, -- Disable borders between verticaly split windows
+      background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
+      term_colors = false, -- Prevent the theme from setting terminal colors
+      eob_lines = false -- Hide the end-of-buffer lines
+    },
+
+    lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+
+    async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
+    custom_highlights = {
+      -- CursorLine = { fg = '#0000FF', underline = false },
+    }, -- Overwrite highlights with your own
+
+    plugins = { -- Here, you can disable(set to false) plugins that you don't use or don't want to apply the theme to
+      trouble = true,
+      nvim_cmp = true,
+      neogit = true,
+      gitsigns = true,
+      git_gutter = true,
+      telescope = true,
+      nvim_tree = true,
+      sidebar_nvim = true,
+      lsp_saga = true,
+      nvim_dap = true,
+      nvim_navic = true,
+      which_key = true,
+      sneak = true,
+      hop = true,
+      indent_blankline = true,
+      nvim_illuminate = true,
+      mini = true,
+    }
+ --    custom_highlights = {
+	-- 	LineNr = { bg = '#FF0000' }
+	-- 	CursorLine = { fg = '#0000FF', underline = true },
+	--
+	-- 	-- This is a list of possible values
+	-- 	YourHighlightGroup = {
+	-- 		fg = "#SOME_COLOR", -- foreground color
+	-- 		bg = "#SOME_COLOR", -- background color
+	-- 		sp = "#SOME_COLOR", -- special color (for colored underlines, undercurls...)
+	-- 		bold = false, -- make group bold
+	-- 		italic = false, -- make group italic
+	-- 		underline = false, -- make group underlined
+	-- 		undercurl = false, -- make group undercurled
+	-- 		underdot = false, -- make group underdotted
+	-- 		underdash = false -- make group underdotted
+	-- 		striketrough = false, -- make group striked trough
+	-- 		reverse = false, -- reverse the fg and bg colors
+	-- 		link = "SomeOtherGroup" -- link to some other highlight group
+	-- 	}
+	-- }
+  })
 end
 
 
