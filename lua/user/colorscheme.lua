b525f05c8495866a38ed93cfb506dbbcfd22e416
anhnguyen <anh.nguyen@knowunity.com>
2022-07-22 00:10:58 +0200

    add catpuccin colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,104 @@
 -- local colorscheme = "kanagawa"
 -- local colorscheme = "tokyonight"
-local colorscheme = "cobalt2"
+-- local colorscheme = "cobalt2"
 -- local colorscheme = "material"
 -- local colorscheme = "nightfly"
+local colorscheme = "catppuccin"
+
+-- catppuccin
+if colorscheme == "catppuccin" then
+  vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+
+  require("catppuccin").setup({
+    -- dim_inactive = {
+    --   enabled = false,
+    --   shade = "dark",
+    --   percentage = 0.15,
+    -- },
+    -- transparent_background = false,
+    term_colors = false,
+    compile = {
+      enabled = true,
+      path = vim.fn.stdpath "cache" .. "/catppuccin",
+    },
+    -- styles = {
+    --   comments = { "italic" },
+    --   conditionals = { "italic" },
+    --   loops = {},
+    --   functions = {},
+    --   keywords = {},
+    --   strings = {},
+    --   variables = {},
+    --   numbers = {},
+    --   booleans = {},
+    --   properties = {},
+    --   types = {},
+    --   operators = {},
+    -- },
+    integrations = {
+      treesitter = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          -- errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+        },
+      },
+      coc_nvim = false,
+      lsp_trouble = false,
+      cmp = true,
+      lsp_saga = false,
+      gitgutter = true,
+      gitsigns = true,
+      -- leap = false,
+      telescope = true,
+      -- nvimtree = {
+      --   enabled = true,
+      --   show_root = true,
+      --   transparent_panel = false,
+      -- },
+      neotree = {
+        enabled = true,
+        show_root = true,
+        transparent_panel = false,
+      },
+      dap = {
+        enabled = false,
+        enable_ui = false,
+      },
+      which_key = true,
+      indent_blankline = {
+        enabled = true,
+        colored_indent_levels = true,
+      },
+      dashboard = true,
+      neogit = false,
+      vim_sneak = false,
+      fern = false,
+      barbar = false,
+      bufferline = true,
+      markdown = true,
+      lightspeed = false,
+      ts_rainbow = false,
+      hop = true,
+      notify = true,
+      telekasten = true,
+      symbols_outline = true,
+      mini = false,
+      aerial = false,
+      vimwiki = false,
+      beacon = false,
+    },
+  })
+end
 
 -- nightfly
 if colorscheme == "nightfly" then
