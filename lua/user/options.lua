06c994bdc94b6f58c18678651e0df34cf114c2b3
anhnguyen <anh.nguyen@knowunity.com>
2022-07-30 12:41:21 +0200

    vimwiki setting

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -19,13 +19,30 @@ vim.opt.termguicolors = true -- set term gui colors (most terminals support this
 vim.opt.timeoutlen = 300 -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.ttimeoutlen = 0
 vim.opt.undofile = true -- enable persistent undo
-vim.opt.updatetime = 400 -- faster completion (4000ms default)
+vim.opt.updatetime = 300 -- faster completion (4000ms default)
 vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2 -- insert 2 spaces for a tab
 vim.opt.cursorline = true -- highlight the current line
 
+vim.opt.lazyredraw = true
+
+-- Vim Wiki settings
+vim.opt.compatible = false
+-- vim.opt.syntax = true
+vim.cmd[[
+ filetype plugin on
+ syntax on
+]]
+
+vim.g.vimwiki_list = {{path = '~/Documents/vimwiki', syntax = 'markdown', ext = '.md'}}
+vim.g.vimwiki_markdown_link_ext = 1
+vim.g.markdown_folding = 1
+
+-- vim.g.vimwiki_ext2syntax = {'.md': 'markdown', '.markdown': 'markdown', '.mdown': 'markdown'}
+
+-- remove cursorline on inactive window
 local group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
 local set_cursorline = function(event, value, pattern)
   vim.api.nvim_create_autocmd(event, {
