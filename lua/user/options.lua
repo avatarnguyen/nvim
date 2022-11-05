26dfef34b7f43964cd7103cbea42af674fa43cc9
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2022-11-05 14:59:23 +0100

    tweak timing

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,6 +1,6 @@
 vim.opt.backup = false -- creates a backup file
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
-vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
+--vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
 vim.opt.fileencoding = "utf-8" -- the encoding written to a file
@@ -17,17 +17,17 @@ vim.opt.splitbelow = true -- force all horizontal splits to go below current win
 vim.opt.splitright = true -- force all vertical splits to go to the right of current window
 vim.opt.swapfile = false -- creates a swapfile
 vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
-vim.opt.timeoutlen = 250 -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.timeoutlen = 300 -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.ttimeoutlen = 0
 vim.opt.undofile = true -- enable persistent undo
-vim.opt.updatetime = 500 -- faster completion (4000ms default)
+vim.opt.updatetime = 350 -- faster completion (4000ms default)
 vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2 -- insert 2 spaces for a tab
 
 vim.opt.lazyredraw = false
-vim.opt.shell = "/bin/bash"
+--[[ vim.opt.shell = "/bin/bash" ]]
 
 vim.opt.number = true -- set numbered lines
 vim.opt.laststatus = 3
