1c7b48be955147ae9d91e28a00a45bec35806abf
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2022-12-11 18:37:48 +0100

    speed up git blame

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -20,13 +20,13 @@ gitsigns.setup {
   --   follow_files = true,
   -- },
   -- attach_to_untracked = true,
-  -- current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-  -- current_line_blame_opts = {
-  --   virt_text = true,
-  --   virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-  --   delay = 1000,
-  --   ignore_whitespace = false,
-  -- },
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    delay = 400,
+    ignore_whitespace = false,
+  },
   -- current_line_blame_formatter_opts = {
   --   relative_time = false,
   -- },
