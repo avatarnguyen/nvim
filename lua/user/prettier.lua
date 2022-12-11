ab7edc324b6db6b6e2250206440d03a3c1b581c7
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2022-12-11 18:35:11 +0100

    add frontend plugins

---
+++ lua/user/prettier.lua
@@ -0,0 +1,28 @@
+local status, prettier = pcall(require, "prettier")
+if (not status) then return end
+prettier.setup({
+  bin = 'prettier',
+  filetypes = {
+    "css",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "json",
+    "scss",
+    "less"
+  },
+  ["null-ls"] = {
+    condition = function()
+      return prettier.config_exists({
+        -- if `false`, skips checking `package.json` for `"prettier"` key
+        check_package_json = true,
+      })
+    end,
+    runtime_condition = function(params)
+      -- return false to skip running prettier
+      return true
+    end,
+    timeout = 5000,
+  }
+})
