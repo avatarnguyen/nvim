64d05c9e795215459caef87a60ecad240ad26c9a
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2022-12-18 10:45:58 +0100

    add incremental selection

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,37 +1,47 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		-- disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = true,
-	},
-	autotag = {
-		enable = true,
-		disable = { "xml" },
-	},
-	rainbow = {
-		enable = true,
-		colors = {
-			"Gold",
-			"Orchid",
-			"DodgerBlue",
-			"Cornsilk",
-			"Salmon",
-			"LawnGreen",
-		},
-		-- disable = { "html" },
-	},
+  ensure_installed = "all", -- one of "all" or a list of languages
+  -- ensure_installed = { "dart", "lua", "go", "help" },
+  ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn", -- set to `false` to disable one of the mappings
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    -- disable = { "css" }, -- list of language that will be disabled
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = { enable = true, disable = { "python", "css" } },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = true,
+  },
+  autotag = {
+    enable = true,
+    disable = { "xml" },
+  },
+  rainbow = {
+    enable = true,
+    colors = {
+      "Gold",
+      "Orchid",
+      "DodgerBlue",
+      "Cornsilk",
+      "Salmon",
+      "LawnGreen",
+    },
+    -- disable = { "html" },
+  },
 })
