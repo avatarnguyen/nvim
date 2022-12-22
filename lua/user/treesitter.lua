4b98c5b512cd80c11c001a60564be5a1c0ec7e15
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2022-12-22 22:32:59 +0100

    add selection keymaps

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -10,10 +10,10 @@ configs.setup({
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = "gnn", -- set to `false` to disable one of the mappings
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
+      init_selection = "gn", -- set to `false` to disable one of the mappings
+      node_incremental = "]v",
+      node_decremental = "[v",
+      scope_incremental = ";n",
     },
   },
   highlight = {
