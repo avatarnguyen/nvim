3a143e5628839869e519580ec1a9b2d6837fc86c
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2022-12-28 12:54:17 +0100

    use tokyonight night

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -154,8 +154,8 @@ if colorscheme == "tokyonight" then
     -- or leave it empty to use the default settings
     -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
     -- style = "moon",
-    style = "storm",
-    -- style = "night",
+    -- style = "storm",
+    style = "night",
     transparent = false, -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
     styles = {
