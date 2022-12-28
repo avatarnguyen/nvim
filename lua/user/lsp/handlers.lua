e43cbfe09c5d4497252bd1f4b34aa6f8d7af24ad
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2022-12-28 12:54:51 +0100

    use lspsage peek def

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -64,7 +64,7 @@ local function lsp_keymaps(bufnr)
   local opts = { noremap = true, silent = true }
   local keymap = vim.api.nvim_buf_set_keymap
   keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  keymap(bufnr, "n", "gr", "<cmd>Lspsaga peek_definition<CR>", opts)
+  -- keymap(bufnr, "n", "gr", "<cmd>Lspsaga peek_definition<CR>", opts)
   keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
   keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 
