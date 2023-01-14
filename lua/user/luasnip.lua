6aa4dd582c564fe3456c34e633019df608de2a37
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2023-01-15 00:43:24 +0100

    knowunity spacer snippets

--- lua/user/luasnip.lua
+++ lua/user/luasnip.lua
@@ -62,7 +62,7 @@ ls.add_snippets(nil, {
     ls.parser.parse_snippet("ssi", "final screenSize = MediaQuery.of(context).size;"),
     ls.parser.parse_snippet("_si", "final screenSize = MediaQuery.of(context).size;"),
     ls.parser.parse_snippet("tda", "//TODO(@anh): "),
-    ls.parser.parse_snippet("tdr", "//TODO(@anh): remove before push"),
+    ls.parser.parse_snippet("tdr", "//FIXME (@anh): remove before push"),
     ls.parser.parse_snippet("tdn", "//TODO(@anh): implement on next mr"),
     ls.parser.parse_snippet("tiu", "// ignore: unused-code"),
 
@@ -74,6 +74,15 @@ ls.add_snippets(nil, {
     ls.parser.parse_snippet("_bs32", "BricksSizes.s32"),
     ls.parser.parse_snippet("_bs40", "BricksSizes.s40"),
     ls.parser.parse_snippet("_bs64", "BricksSizes.s64"),
+
+    ls.parser.parse_snippet(",bs4", "BricksSizes.s4"),
+    ls.parser.parse_snippet(",bs8", "BricksSizes.s8"),
+    ls.parser.parse_snippet(",bs12", "BricksSizes.s12"),
+    ls.parser.parse_snippet(",bs16", "BricksSizes.s16"),
+    ls.parser.parse_snippet(",bs24", "BricksSizes.s24"),
+    ls.parser.parse_snippet(",bs32", "BricksSizes.s32"),
+    ls.parser.parse_snippet(",bs40", "BricksSizes.s40"),
+    ls.parser.parse_snippet(",bs64", "BricksSizes.s64"),
     -- Icon Sizes
     ls.parser.parse_snippet("_bis4", "BricksIconSizes.s4"),
     ls.parser.parse_snippet("_bis8", "BricksIconSizes.s8"),
@@ -85,20 +94,6 @@ ls.add_snippets(nil, {
     ls.parser.parse_snippet("_bis64", "BricksIconSizes.s64"),
 
     -- Spacer
-    ls.parser.parse_snippet("bv4", "const BricksSpacer.v4();"),
-    ls.parser.parse_snippet("bv8", "const BricksSpacer.v8();"),
-    ls.parser.parse_snippet("bv16", "const BricksSpacer.v16();"),
-    ls.parser.parse_snippet("bv32", "const BricksSpacer.v32();"),
-    ls.parser.parse_snippet("bv40", "const BricksSpacer.v40();"),
-    ls.parser.parse_snippet("bv48", "const BricksSpacer.v48();"),
-    ls.parser.parse_snippet("bv64", "const BricksSpacer.v64();"),
-    ls.parser.parse_snippet("bh4", "const BricksSpacer.h4();"),
-    ls.parser.parse_snippet("bh8", "const BricksSpacer.h8();"),
-    ls.parser.parse_snippet("bh16", "const BricksSpacer.h16();"),
-    ls.parser.parse_snippet("bh32", "const BricksSpacer.h32();"),
-    ls.parser.parse_snippet("bh40", "const BricksSpacer.h40();"),
-    ls.parser.parse_snippet("bh48", "const BricksSpacer.h48();"),
-    ls.parser.parse_snippet("bh64", "const BricksSpacer.h64();"),
     ls.parser.parse_snippet("rcon", "ColoredBox(color: Colors.red,);"),
     snip("csl",
       fmta("class <> extends ConsumerWidget {\n\tconst <>({super.key});\n\t@override\tWidget build(BuildContext context, WidgetRef ref) {\n\t\treturn Container();\n\t}\n}"
