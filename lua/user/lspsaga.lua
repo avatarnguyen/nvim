080b2861605c569abb4e6f334c5bec8d32854351
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2023-01-21 20:38:22 +0100

    update lspsaga config

--- lua/user/lspsaga.lua
+++ lua/user/lspsaga.lua
@@ -5,6 +5,37 @@ if not status_ok then
 end
 
 saga.setup {
+  ui = {
+    -- currently only round theme
+    theme = 'round',
+    -- border type can be single,double,rounded,solid,shadow.
+    border = 'solid',
+    winblend = 0,
+    expand = '',
+    collapse = '',
+    preview = ' ',
+    code_action = '💡',
+    diagnostic = '🐞',
+    incoming = ' ',
+    outgoing = ' ',
+    colors = {
+      --float window normal background color
+      normal_bg = '#1d1536',
+      --title background color
+      title_bg = '#afd700',
+      red = '#e95678',
+      magenta = '#b33076',
+      orange = '#FF8700',
+      yellow = '#f7bb3b',
+      green = '#afd700',
+      cyan = '#36d0e0',
+      blue = '#61afef',
+      purple = '#CBA6F7',
+      white = '#d1d4cf',
+      black = '#1c1c19',
+    },
+    kind = {},
+  },
   lightbulb = {
     enable = true,
     enable_in_insert = true,
@@ -13,7 +44,7 @@ saga.setup {
     virtual_text = false,
   },
   code_action = {
-    num_shortcut = true,
+    num_shortcut = false,
     keys = {
       quit = 'q',
       exec = '<CR>',
