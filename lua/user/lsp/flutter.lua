f28099ac594bd5cafcad0bcfc33258601aa51ac0
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2023-02-04 00:39:19 +0000

    add background color for transparent nvim

--- lua/user/lsp/flutter.lua
+++ lua/user/lsp/flutter.lua
@@ -47,6 +47,8 @@ flutter.setup({
     color = { -- show the derived colours for dart variables
       enabled = true,
       background = false, -- highlight the background
+      -- background_color = nil, -- required, when background is transparent 
+      background_color = { r = 19, g = 17, b = 24},
       foreground = false, -- highlight the foreground
       virtual_text = true, -- show the highlight using virtual text
       virtual_text_str = "■■", -- the virtual text character to highlight
