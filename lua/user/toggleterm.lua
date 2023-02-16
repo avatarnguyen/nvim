4a298da7640a9665db37a28d4dba0c25d509a48f
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2023-02-16 22:49:26 +0000

    fullsize lazygit

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -1,28 +1,28 @@
 local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then
-	return
+  return
 end
 
 toggleterm.setup({
-	size = 10,
-	open_mapping = [[<c-\>]],
-	hide_numbers = true,
-	shade_terminals = false,
-	shading_factor = 2,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	-- direction = 'float',
-	direction = 'horizontal',
-	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-	},
+  size = 10,
+  open_mapping = [[<c-\>]],
+  hide_numbers = true,
+  shade_terminals = false,
+  shading_factor = 2,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  direction = 'float',
+  -- direction = 'horizontal',
+  close_on_exit = true,
+  shell = vim.o.shell,
+  float_opts = {
+    border = "curved",
+  },
 })
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
+  local opts = { noremap = true }
   -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
@@ -32,15 +32,58 @@ end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
+local function get_buf_size()
+  local cbuf = vim.api.nvim_get_current_buf()
+  local bufinfo = vim.tbl_filter(function(buf)
+        return buf.bufnr == cbuf
+      end, vim.fn.getwininfo(vim.api.nvim_get_current_win()))[1]
+  if bufinfo == nil then
+    return { width = -1, height = -1 }
+  end
+  return { width = bufinfo.width, height = bufinfo.height }
+end
+
+--- Get the dynamic terminal size in cells
+---@param direction number
+---@param size integer
+---@return integer
+local function get_dynamic_terminal_size(direction, size)
+  size = size
+  if direction ~= "float" and tostring(size):find(".", 1, true) then
+    size = math.min(size, 1.0)
+    local buf_sizes = get_buf_size()
+    local buf_size = direction == "horizontal" and buf_sizes.height or buf_sizes.width
+    return buf_size * size
+  else
+    return size
+  end
+end
+
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float", size = 40 })
+local lazygit = Terminal:new {
+  cmd = "lazygit",
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "curved",
+    width = 100000,
+    height = 100000,
+  },
+  on_open = function(_)
+    vim.cmd "startinsert!"
+  end,
+  on_close = function(_)
+  end,
+  count = 99,
+}
+
 local flutterTranslationUpdate = Terminal:new({ cmd = "ft && fpg", hidden = false })
 
+
 function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
+  lazygit:toggle()
 end
 
 function _RUN_TRANSLATION()
-	flutterTranslationUpdate:toggle()
+  flutterTranslationUpdate:toggle()
 end
-
