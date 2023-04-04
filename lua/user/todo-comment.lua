1ae37878e7565872c994a6a4373b59b5c71c64c8
anh.nguyen.knowunity <anh.nguyen@knowunity.com>
2023-04-04 11:56:26 +0200

    add exclude folder for todo-comment

--- lua/user/todo-comment.lua
+++ lua/user/todo-comment.lua
@@ -1,54 +1,61 @@
 local todo_ok, todo = pcall(require, "todo-comments")
 if not todo_ok then
-  vim.api.nvim_err_writeln "Failed to load TODO Comment"
-  return
+	vim.api.nvim_err_writeln("Failed to load TODO Comment")
+	return
 end
 
-todo.setup(
-  {
-    signs = true,
-    sign_priority = 30,
-    keywords = {
-      FIX = {
-        icon = " ",
-        color = "error",
-        alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
-      },
-      TODO = { icon = " ", color = "info" },
-      HACK = { icon = " ", color = "warning" },
-      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-      NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-    },
-    merge_keywords = true,
-    highlight = {
-      before = "",
-      keyword = "wide",
-      after = "fg",
-      pattern = [[.*<(KEYWORDS)\s*:]],
-      comments_only = true,
-      max_line_len = 400,
-      exclude = {},
-    },
-    colors = {
-      error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-      warning = { "DiagnosticWarning", "WarningMsg", "#FBBF24" },
-      info = { "DiagnosticInfo", "#2563EB" },
-      hint = { "DiagnosticHint", "#10B981" },
-      default = { "Identifier", "#7C3AED" },
-    },
-    search = {
-      command = "rg",
-      args = {
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        --[[ "--glob=!node_modules, !packages, !l10n, !ios, !android, !windows", ]]
-        "--glob=!packages",
-      },
-      pattern = [[\b(KEYWORDS):]],
-    },
-  }
-)
+todo.setup({
+	signs = true,
+	sign_priority = 30,
+	keywords = {
+		FIX = {
+			icon = " ",
+			color = "error",
+			alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
+		},
+		TODO = { icon = " ", color = "info" },
+		HACK = { icon = " ", color = "warning" },
+		WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+		PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+		NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+	},
+	merge_keywords = true,
+	highlight = {
+		multiline = true, -- enable multine todo comments
+		multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+		multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+		before = "", -- "fg" or "bg" or empty
+		keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+		after = "fg", -- "fg" or "bg" or empty
+		pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+		comments_only = true, -- uses treesitter to match keywords in comments only
+		max_line_len = 400, -- ignore lines longer than this
+		exclude = { "yaml", "txt", ".yaml", ".txt", "*.yaml" }, -- list of file types to exclude highlighting
+	},
+	colors = {
+		error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+		warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+		info = { "DiagnosticInfo", "#2563EB" },
+		hint = { "DiagnosticHint", "#10B981" },
+		default = { "Identifier", "#7C3AED" },
+		test = { "Identifier", "#FF00FF" },
+	},
+	search = {
+		command = "rg",
+		args = {
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--glob=!packages",
+			"--glob=!windows",
+			"--glob=!linux",
+			"--glob=!*.yaml",
+		},
+		-- regex that will be used to match keywords.
+		-- don't replace the (KEYWORDS) placeholder
+		pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+		-- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+	},
+})
